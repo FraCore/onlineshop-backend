36bd6823db18dbe54cac0449d1dea2216ec912b6
Felix Endres (ImbaSoft) <Felix.Endres@imbasoft.at>
2023-09-16 22:48:41 +0200

    check product object valid change mapping specific error handling

--- src/main/java/com/onlineshop/onlineshopbackendapplication/controller/ProductController.java
+++ src/main/java/com/onlineshop/onlineshopbackendapplication/controller/ProductController.java
@@ -2,30 +2,45 @@ package com.onlineshop.onlineshopbackendapplication.controller;
 
 import com.onlineshop.onlineshopbackendapplication.model.Product;
 import com.onlineshop.onlineshopbackendapplication.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(path = "product")
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @PostMapping(path="/addProduct", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody HttpStatus addNewProduct(@RequestBody Product product) {
+    @PostMapping(path="/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<String> addNewProduct(@Valid @RequestBody Product product) {
         if(productRepository.findById(product.getProduct_id()).isPresent()) {
-            return HttpStatus.CONFLICT;
+            return new ResponseEntity<>("Product already exists", HttpStatus.CONFLICT);
         }
-        productRepository.save(product);
-        return HttpStatus.CREATED;
+        try {
+            productRepository.save(product);
+            return new ResponseEntity<>("Product added successfully", HttpStatus.CREATED);
+
+        }catch(Exception e) {
+            return new ResponseEntity<>("An error occurred",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping(path="/getAllProducts")
-    public @ResponseBody Iterable<Product> getAllProducts() {
-        return productRepository.findAll();
+    @PostMapping(path="/update/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ResponseEntity<String> updateProduct(@PathVariable Integer id,@Valid @RequestBody Product product) {
+        if(productRepository.findById(id).isPresent()) {
+            try {
+                product.setProduct_id(id);
+                productRepository.save(product);
+                return new ResponseEntity<>("Product successfully updated", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("An error occurred",HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<>(String.format("Product with id: %d not found", id), HttpStatus.NOT_FOUND);
     }
-
-
 }
